a52d50647765cb874614f038a4ef1a45cf8fa2fd
Jeff Ripke <jripke74@gmail.com>
2017-11-25 12:44:12 -0800

    initial commit

---
+++ CalculatorTakeII/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CalculatorTakeII
+//
+//  Created by Jeff Ripke on 11/25/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputDisplayLabel: UILabel!
+    
+    @IBAction func oneButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func twoButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func plussButton(_ sender: UIButton) {
+        
+    }
+    
+}

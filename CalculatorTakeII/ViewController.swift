ccb428b217b44307f4e913f0d8c00b663e8cc4eb
Jeff Ripke <jripke74@gmail.com>
2017-11-30 16:37:09 -0800

    added digits and formula to labels

--- CalculatorTakeII/ViewController.swift
+++ CalculatorTakeII/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         case zero = "0"
         case one = "1"
         case two = "2"
+        case three = "3"
+        case four = "4"
+        case five = "5"
+        case six = "6"
+        case seven = "7"
+        case eight = "8"
+        case nine = "9"
     }
     
     enum Operators: String {
@@ -24,45 +31,82 @@ class ViewController: UIViewController {
     
     var digits: [Digits] = []
     var operators: [Operators] = []
+    var colector = ""
     
     
     @IBOutlet weak var formulaLabel: UILabel!
     @IBOutlet weak var outputLabel: UILabel!
     
     @IBAction func zeroButton(_ sender: UIButton) {
+        digits.append(.zero)
+        colector += "0"
+        formulaLabel.text = colector
     }
     
     @IBAction func oneButton(_ sender: UIButton) {
+        digits.append(.one)
+        colector += "1"
+        formulaLabel.text = colector
     }
 
     @IBAction func twoButton(_ sender: UIButton) {
+        digits.append(.two)
+        colector += "2"
+        formulaLabel.text = colector
     }
     
     @IBAction func threeButton(_ sender: UIButton) {
+        digits.append(.three)
+        colector += "3"
+        formulaLabel.text = colector
     }
     
     @IBAction func fourButton(_ sender: UIButton) {
+        digits.append(.four)
+        colector += "4"
+        formulaLabel.text = colector
     }
     
     @IBAction func fiveButton(_ sender: UIButton) {
+        digits.append(.five)
+        colector += "5"
+        formulaLabel.text = colector
     }
     
     @IBAction func sixButton(_ sender: UIButton) {
+        digits.append(.six)
+        colector += "6"
+        formulaLabel.text = colector
     }
     
     @IBAction func sevenButton(_ sender: UIButton) {
+        digits.append(.seven)
+        colector += "7"
+        formulaLabel.text = colector
     }
     
     @IBAction func eightButton(_ sender: UIButton) {
+        digits.append(.eight)
+        colector += "8"
+        formulaLabel.text = colector
     }
     
     @IBAction func nineButton(_ sender: UIButton) {
+        digits.append(.nine)
+        colector += "9"
+        formulaLabel.text = colector
     }
     
     @IBAction func plusButton(_ sender: UIButton) {
+        operators.append(.plus)
+        colector += "+"
+        formulaLabel.text = colector
     }
     
     @IBAction func minusButton(_ sender: UIButton) {
+        operators.append(.minus)
+        colector += "-"
+        formulaLabel.text = colector
     }
     
     @IBAction func equalButton() {
@@ -71,8 +115,10 @@ class ViewController: UIViewController {
         if let leftOperand = Int(digits[0].rawValue), let rightOperand = Int(digits[1].rawValue) {
             switch centerOperator {
             case .plus:
+                outputLabel.text = "\(leftOperand) + \(rightOperand) = \(leftOperand + rightOperand)"
                 print("\(leftOperand) + \(rightOperand) = \(leftOperand + rightOperand)")
             case .minus:
+                outputLabel.text = "\(leftOperand) + \(rightOperand) = \(leftOperand - rightOperand)"
                 print("\(leftOperand) - \(rightOperand) = \(leftOperand - rightOperand)")
             case .equals:
                 print("\(leftOperand) + \(rightOperand) = \(leftOperand + rightOperand)")
